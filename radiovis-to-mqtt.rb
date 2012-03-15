cec8faad166ffe18dfb5755c3f8383f57deb8b09
Nicholas J Humfrey <njh@aelius.com>
2012-03-15 22:52:34 +0000

    Moved configuration setting into a YAML configuration file.

--- radiovis-to-mqtt.rb
+++ radiovis-to-mqtt.rb
@@ -5,9 +5,8 @@ require "bundler"
 
 Bundler.require(:default)
 
-STOMP_HOST = 'vis.musicradio.com'
-MQTT_HOST = 'test.mosquitto.org'
-
+# Load the configuration file
+CONFIG = YAML::load_file('config.yml')
 
 # Make STDOUT unbuffered
 STDOUT.sync = true
@@ -18,29 +17,47 @@ module StompClient
   def connection_completed
     connect
   end
-  
+
   def message_callback(&block)
     @message_callback = block
   end
 
+  def connected_callback(&block)
+    @connected_callback = block
+  end
+
   def receive_msg msg
     if msg.command == "CONNECTED"
-      subscribe '/topic/fm/ce1/c479/09580/image' # Capital FM
-      subscribe '/topic/fm/ce1/c479/09580/text' # Capital FM
+      @connected_callback.call(msg) unless @connected_callback.nil?
     elsif msg.command == "MESSAGE"
       @message_callback.call(msg) unless @message_callback.nil?
     end
   end
 end
 
+
 EM.run do
-  mqtt = EventMachine::MQTT::ClientConnection.connect(MQTT_HOST)
-  stomp = EM.connect(STOMP_HOST, 61613, StompClient)
-  
+  mqtt = EventMachine::MQTT::ClientConnection.connect(
+    CONFIG['mqtt']['host'],
+    CONFIG['mqtt']['port']
+  )
+  stomp = EM.connect(
+    CONFIG['radiovis']['host'],
+    CONFIG['radiovis']['port'],
+    StompClient
+  )
+
+  stomp.connected_callback do
+    CONFIG['identifiers'].each do |identifier|
+      stomp.subscribe "/topic/#{identifier}/image"
+      stomp.subscribe "/topic/#{identifier}/text"
+    end
+  end
+
   stomp.message_callback do |msg|
     topic = msg.header['destination'].sub(%r(^/topic/), 'radiovis/')
     body = msg.body.sub(/^([A-Z]+) /, '')
     p [topic, body]
     mqtt.publish(topic, body, retain=true)
-  end  
+  end
 end
