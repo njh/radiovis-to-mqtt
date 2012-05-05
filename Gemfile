2a6718f5985944ee569b55c10307b553080a0568
Nicholas J Humfrey <njh@aelius.com>
2012-05-05 14:51:39 +0100

    Added support for subscribing on multiple server and looking up those servers using RadioDNS.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "http://rubygems.org"
 
 gem 'eventmachine'
+gem 'radiodns'
 gem 'em-mqtt'
--- radiovis-to-mqtt.rb
+++ radiovis-to-mqtt.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
-require "bundler"
+require 'bundler'
+require 'pp'
 
 Bundler.require(:default)
 
@@ -10,6 +11,24 @@ CONFIG = YAML::load_file('config.yml')
 
 # Make STDOUT unbuffered
 STDOUT.sync = true
+
+# First, resolve the services using RadioDNS
+services = {}
+CONFIG['radiovis'].each do |topic|
+  dns = topic.split('/').reverse.join('.') + '.radiodns.org'
+  begin
+    radiovis = RadioDNS::Resolver.resolve(dns).radiovis
+    services[radiovis.host] ||= {}
+    services[radiovis.host]['port'] = radiovis.port
+    services[radiovis.host]['topics'] ||= []
+    services[radiovis.host]['topics'] << "/topic/#{topic}/image"
+    services[radiovis.host]['topics'] << "/topic/#{topic}/text"
+  rescue Resolv::ResolvError
+    puts "No RadioVis service found for #{dns}"
+  end
+end
+
+pp services
 
 module StompClient
   include EM::Protocols::Stomp
@@ -27,9 +46,9 @@ module StompClient
   end
 
   def receive_msg msg
-    if msg.command == "CONNECTED"
+    if msg.command == 'CONNECTED'
       @connected_callback.call(msg) unless @connected_callback.nil?
-    elsif msg.command == "MESSAGE"
+    elsif msg.command == 'MESSAGE'
       @message_callback.call(msg) unless @message_callback.nil?
     end
   end
@@ -41,23 +60,22 @@ EM.run do
     CONFIG['mqtt']['host'],
     CONFIG['mqtt']['port']
   )
-  stomp = EM.connect(
-    CONFIG['radiovis']['host'],
-    CONFIG['radiovis']['port'],
-    StompClient
-  )
 
-  stomp.connected_callback do
-    CONFIG['identifiers'].each do |identifier|
-      stomp.subscribe "/topic/#{identifier}/image"
-      stomp.subscribe "/topic/#{identifier}/text"
+  services.each_pair do |host,args|
+    stomp = EM.connect(
+      host, args['port'], StompClient
+    )
+
+    stomp.connected_callback do
+      args['topics'].each do |topic|
+        stomp.subscribe topic
+      end
     end
-  end
 
-  stomp.message_callback do |msg|
-    topic = msg.header['destination'].sub(%r(^/topic/), 'radiovis/')
-    body = msg.body.sub(/^([A-Z]+) /, '')
-    p [topic, body]
-    mqtt.publish(topic, body, retain=true)
+    stomp.message_callback do |msg|
+      topic = msg.header['destination'].sub(%r(^/topic/), 'radiovis/')
+      body = msg.body.sub(/^([A-Z]+) /, '')
+      mqtt.publish(topic, body, retain=true)
+    end
   end
 end
